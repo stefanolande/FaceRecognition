3aa8c1d5abdcd394b0c8ae60bf0e7ea6608a11f6
Stefano Lande <landeste@gmail.com>
2016-12-03 12:04:29 +0100

    commenti tradotti in italiano

--- eigenfaces.m
+++ eigenfaces.m
@@ -2,7 +2,8 @@ clear;
 input_dir = 'training/processed/';
 image_dims = [192, 168];
 
-soglia = 0.00172; %Questa Ã¨ la sogllia migliore che abbiamo ricavato mediante il cimpromesso dato da TN e NT
+%Questa è la sogllia migliore ricavata mediante compromesso tra da FP e FN
+soglia = 0.00172; 
 
 filenames = dir(fullfile(input_dir, '*.pgm'));
 num_images = numel(filenames);
@@ -20,45 +21,50 @@ for n = 1:num_images
     img = imread(filename);
     images(:, n) = img(:);
 end
+fprintf('Lettura immagini completata\n');
 
-% steps 1 and 2: find the mean image and the mean-shifted input images
+
+% step 1 and 2: trova la faccia media e sottraila ad ogni immagine
 mean_face = mean(images, 2);
 shifted_images = images - repmat(mean_face, 1, num_images);
 fprintf('step 1 2 done\n');
  
-% steps 3 and 4: calculate the ordered eigenvectors and eigenvalues
+% step 3 and 4: calcola gli autovettori e gli autovalori ordinati
 [evectors, score, evalues] = pca(images');
 fprintf('step 3 4 done\n');
  
-% step 5: only retain the top 'num_eigenfaces' eigenvectors (i.e. the principal components)
+% step 5: teniamo solo i primi autovettori
 num_eigenfaces = 20;
 evectors = evectors(:, 1:num_eigenfaces);
 fprintf('step 5 done\n');
  
-% step 6: project the images into the subspace to generate the feature vectors
+% step 6: calcola le feature di ogni faccia proiettandole sugli autovettori
 features = evectors' * shifted_images;
 fprintf('step 6 done\n');
 
 name = 'E la, il primo ciclo lo fai per forza';
 while(strcmp(name, 'esci')==0)    
    
-    [filename] = imgetfile(); %This function permits to choose an input image by graphic interface
-    % calculate the similarity of the input to each training image
+    [filename] = imgetfile(); %Scelta dell'immagine da gui
+    
+    %calcola la similarità dell'immagine in input con ogni immagine nel
+    %training set
     input_image = double(imread(filename));
     [pathstr_notUsed,current_name] = fileparts(filename);
     fprintf('Immagine scelta: %s \n',current_name);
     feature_vec = evectors' * (input_image(:) - mean_face);
     similarity_score = arrayfun(@(n) 1 / (1 + norm(features(:,n) - feature_vec)), 1:num_images);
 
-    % find the image with the highest similarity
+    %trova l'immagine con la similarità più alta
     [match_score, match_ix] = max(similarity_score);
 
-    % display the result
+    %mostra il risultato
     figure, imshow([mat2gray(input_image) mat2gray(reshape(images(:,match_ix), image_dims))]);
-    if(match_score>soglia) %If the match-score of the more similar image is higher than the threshold, the image is Accepted
+    if(match_score>soglia)
+        %se la similarità è più alta della soglia accettiamo il riconoscimento
         title(sprintf('matches %s, score %f', filenames(match_ix).name, match_score));
         xlabel('Accepted');
-    else %Otherwise the image is rejected
+    else
         title(sprintf('closest %s, score %f', filenames(match_ix).name, match_score));
         xlabel('Rejected');
     end
--- eigenfaces_roc.m
+++ eigenfaces_roc.m
@@ -5,7 +5,6 @@ image_dims = [192, 168];
 filenames = dir(fullfile(input_dir, '*.pgm'));
 num_images = numel(filenames);
 
-%leggi la prima immagine per ottenere le dimensioni
 filename = fullfile(input_dir, filenames(1).name);
 img = imread(filename);
 dim = size(img(:));
@@ -18,21 +17,17 @@ for n = 1:num_images
     images(:, n) = img(:);
 end
 
-% steps 1 and 2: find the mean image and the mean-shifted input images
 mean_face = mean(images, 2);
 shifted_images = images - repmat(mean_face, 1, num_images);
 fprintf('step 1 2 done\n');
  
-% steps 3 and 4: calculate the ordered eigenvectors and eigenvalues
 [evectors, score, evalues] = pca(images');
 fprintf('step 3 4 done\n');
  
-% step 5: only retain the top 'num_eigenfaces' eigenvectors (i.e. the principal components)
 num_eigenfaces = 20;
 evectors = evectors(:, 1:num_eigenfaces);
 fprintf('step 5 done\n');
  
-% step 6: project the images into the subspace to generate the feature vectors
 features = evectors' * shifted_images;
 fprintf('step 6 done\n');
 
@@ -50,13 +45,12 @@ num_testimages = numel(test_filenames);
 ROCv = zeros(num_testimages,2);
 
 for n = 1:num_testimages   
-    % calculate the similarity of the input to each training image
+
     filename = fullfile(input_dir, test_filenames(n).name);
     input_image = double(imread(filename));
     feature_vec = evectors' * (input_image(:) - mean_face);
     similarity_score = arrayfun(@(n) 1 / (1 + norm(features(:,n) - feature_vec)), 1:num_images);
 
-    % find the image with the highest similarity
     [match_score, match_ix] = max(similarity_score);
 
     found_name = filenames(match_ix).name;
