48461376bc3dc9a9627f60ac5e89c330f9204a57
serusisergio <serusisergio@hotmail.it>
2016-12-03 10:20:46 +0100

    "Added input image name, just only the name"

--- eigenfaces.m
+++ eigenfaces.m
@@ -2,7 +2,7 @@ clear;
 input_dir = 'training/processed/';
 image_dims = [192, 168];
 
-soglia = 0.00172;
+soglia = 0.00172; %Questa è la sogllia migliore che abbiamo ricavato mediante il cimpromesso dato da TN e NT
 
 filenames = dir(fullfile(input_dir, '*.pgm'));
 num_images = numel(filenames);
@@ -14,6 +14,7 @@ dim = size(img(:));
 
 images = zeros(dim(1), num_images);
 
+% Mettiamo ogni immagine in una riga della matrice
 for n = 1:num_images
     filename = fullfile(input_dir, filenames(n).name);
     img = imread(filename);
@@ -38,34 +39,31 @@ fprintf('step 5 done\n');
 features = evectors' * shifted_images;
 fprintf('step 6 done\n');
 
-input_dir = 'test/processed/';
+name = 'E la, il primo ciclo lo fai per forza';
+while(strcmp(name, 'esci')==0)    
+   
+    [filename] = imgetfile(); %This function permits to choose an input image by graphic interface
+    % calculate the similarity of the input to each training image
+    input_image = double(imread(filename));
+    [pathstr_notUsed,current_name] = fileparts(filename);
+    fprintf('Immagine scelta: %s \n',current_name);
+    feature_vec = evectors' * (input_image(:) - mean_face);
+    similarity_score = arrayfun(@(n) 1 / (1 + norm(features(:,n) - feature_vec)), 1:num_images);
 
-name = '';
-while(strcmp(name, 'esci')==0)
-    
-    
-    if(strcmp(name , 'esci')==0)
-        [filename] = imgetfile(); %This function permits to choose an input image by graphic interface
-        fprintf('Come immagine hai scelto: %s \n',filename);
-        % calculate the similarity of the input to each training image
-        %filename = fullfile(input_dir, name);
-        input_image = double(imread(filename));
-        feature_vec = evectors' * (input_image(:) - mean_face);
-        similarity_score = arrayfun(@(n) 1 / (1 + norm(features(:,n) - feature_vec)), 1:num_images);
+    % find the image with the highest similarity
+    [match_score, match_ix] = max(similarity_score);
 
-        % find the image with the highest similarity
-        [match_score, match_ix] = max(similarity_score);
-
-        % display the result
-        figure, imshow([mat2gray(input_image) mat2gray(reshape(images(:,match_ix), image_dims))]);
-        if(match_score>soglia) %If the match-score of the more similar image is higher than the threshold, the image is Accepted
-            title(sprintf('matches %s, score %f', filenames(match_ix).name, match_score));
-            xlabel('Accepted');
-        else %Otherwise the image is rejected
-            title(sprintf('closest %s, score %f', filenames(match_ix).name, match_score));
-            xlabel('Rejected');
-        end
+    % display the result
+    figure, imshow([mat2gray(input_image) mat2gray(reshape(images(:,match_ix), image_dims))]);
+    if(match_score>soglia) %If the match-score of the more similar image is higher than the threshold, the image is Accepted
+        title(sprintf('matches %s, score %f', filenames(match_ix).name, match_score));
+        xlabel('Accepted');
+    else %Otherwise the image is rejected
+        title(sprintf('closest %s, score %f', filenames(match_ix).name, match_score));
+        xlabel('Rejected');
     end
-    name = input('Vuoi uscire inserisci esci, altrimenti premi invio: ', 's');
+    
+    name = input('Se vuoi uscire inserisci esci, altrimenti premi invio: ', 's');
 
 end
+fprintf('\nRiconoscimento facciale terminato!\n\n');
