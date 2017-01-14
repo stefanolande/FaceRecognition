db7913ef9b60fcfc1ca6abd728b5339ae23d0b8b
serusisergio <serusisergio@hotmail.it>
2017-01-14 17:53:47 +0100

    "End comment in preprocess.m class "

--- consegna/preprocess.m
+++ consegna/preprocess.m
@@ -1,7 +1,8 @@
 % Questa funzione si occupa di eseguire del preprocessing sull'immagine X,
 % in modo tale da rendere uniformi tutte le immagini e cercando di
-% eliminare i vari rumori.
-
+% eliminare i vari rumori, mediante l'utilizzo della funzione di normalizzazione per trasformata discreta del coseno.
+% Questa funzione riceve in ingresso una immagine X e restituisce una
+% immagine "img" processata come sopra descritto.
 function img=preprocess(X)
     img = DCT_normalization(X); %richiamiamo la funzione DTC_normalization presente nella cartella "libs"
 end
