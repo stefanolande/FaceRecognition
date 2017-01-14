b905f7ba0355403d3f38ff4e74bb9ec0c8bad4bc
serusisergio <serusisergio@hotmail.it>
2017-01-14 16:56:40 +0100

    "End comments in class run_test.m"

--- consegna/run_test.m
+++ consegna/run_test.m
@@ -26,23 +26,29 @@ test_path = '../dataset1/test/processed';
 test_filenames = dir(fullfile(test_path, '*.pgm'));
 num_testimages = numel(test_filenames);
 
-ROCv = zeros(num_testimages,2);
+ROCv = zeros(num_testimages,2); %inizializziamo il vettore ROCv con 0.
 
-for n = 1:num_testimages   
+for n = 1:num_testimages   %Scorro ogni immagine dell'insieme di immagini di test (creato in modo random)
 
-    filename = fullfile(test_path, test_filenames(n).name);
-    probe = double(imread(filename));
+    filename = fullfile(test_path, test_filenames(n).name); 
+    probe = double(imread(filename)); % probe conterrà l'immagine test corrente 
     
-    probe_features = get_features(evectors, mean_face, probe);
-    [match_score, match_image, recognized_class] = recognize(gallery, classes, gallery_features, probe_features);
+    probe_features = get_features(evectors, mean_face, probe); %data l'immagine test corrente, ricaviamo le features dell'immagine stessa
+    %e richiamiamo la funzione recognize, che ci restituisce l'immagine più
+    %simile trovata, la classe(soggetto) che viene riconosciuta ed infine
+    %lo score ottenuto
+    [match_score, match_image, recognized_class] = recognize(gallery, classes, gallery_features, probe_features); 
 
-    true_class = str2double(test_filenames(n).name(6:7));
+    true_class = str2double(test_filenames(n).name(6:7)); %recuperiamo il numero che identifica il soggetto presente nell'immagine 
 
-    recognized = true_class == recognized_class;
+    recognized = true_class == recognized_class;%se il soggetto è riconosciuto in modo giusto, allora recognized per questa immagine del test varrà true
     
-    ROCv(n,1) = match_score;
-    ROCv(n,2) = recognized;
+    ROCv(n,1) = match_score; %salviamo lo score ottenuto nel riconoscere o meno il soggetto
+    ROCv(n,2) = recognized; %salviamo se il soggetto è stato riconosciuto o meno mediante valore booleano (utile per calcolare la curva ROC)
     
 end
-
+% Richiamiamo la funzione roc passando come input il vettore contenente per
+% la posizione che rappresenta una immagine del test set, lo score ottenuto
+% ed un valore booleano indicante se il soggetto è stato riconosciuto o
+% meno
 roc(ROCv);
