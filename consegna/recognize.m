e3aa69ccf0a961eafa95fe0f30d8bf6d831bf9be
serusisergio <serusisergio@hotmail.it>
2017-01-14 17:37:24 +0100

    "End comment in class recognize.m"

--- consegna/recognize.m
+++ consegna/recognize.m
@@ -1,11 +1,17 @@
+%Questa funzione riceve in ingresso la gallery (che contiene le immagini),
+%le classi, che contiene il soggetto che rappresenta ogni immagine, le
+%features di ogni immagine della gallery ed infine le features
+%dell'immagine probe.
+%Questa funzione restituisce l'immagine più simile trovata, la classe
+%dell'immagine (e quindi il soggetto che è rappresentato nell'immagine
+%trovata) ed infine lo score ottenuto
 function [match_score, match_image, class] = recognize(gallery, classes, gallery_features, probe_features)
-%RECOGNIZE Summary of this function goes here
-%   Detailed explanation goes here
 
+gallery_size = size(gallery,2); %recuperiamo il numero delle immagini presenti in gallery
+similarity_scores = zeros(1, gallery_size); %inizializziamo il vettore delle similarità a 0
 
-gallery_size = size(gallery,2);
-similarity_scores = zeros(1, gallery_size);
-
+%calcoliamo lo score di similarità tra ogni immagine presente in gallery e l'immagine di
+%probe (rappresentata mediante le sue caratteristiche)
 for i = 1:gallery_size
     similarity_scores(i) = 1 / (1 + norm(gallery_features(:,i) - probe_features));
 end
@@ -14,8 +20,11 @@ end
 %trova l'indice dell'immagine con la similarita' piu' alta
 [match_score, match_id] = max(similarity_scores);
 
+%salva l'immagine con la similarità piu alta (che restituirà al chiamante)
 match_image = gallery(:,match_id);
 
+%trova la classe del soggetto presente nell'immagine più simile che abbiamo
+%trovato
 class = classes(match_id);
 
 end
