7c7debdc2ef0e9a0746cd48a386876fedf8cad95
serusisergio <serusisergio@hotmail.it>
2017-01-14 17:58:36 +0100

    "End comments in get_features class"

--- consegna/get_features.m
+++ consegna/get_features.m
@@ -1,5 +1,7 @@
-% Aggiungi descrizione_ ToDo
+% Questa funzione riceve in ingresso una immagine (probe), la media delle
+% facce di gallery e gli autovettori di gallery. 
+% Restituisce al chiamante le features dell'image probe.
 function [ probe_features ] = get_features( evectors, mean_face, probe )
-    probe_features = evectors' * (probe(:) - mean_face);
+    probe_features = evectors' * (probe(:) - mean_face); %calcola òle features dell'immagine probe
 end
 
