a3cca05c1aa3b52c85ef4c5cfbf697ee72de0971
serusisergio <serusisergio@hotmail.it>
2017-01-14 16:25:57 +0100

    End comments in class run.m

--- consegna/run.m
+++ consegna/run.m
@@ -23,23 +23,26 @@ soglia = 0.00049;
 % autovettori ed infine le feature della gallery
 [mean_face, evectors,  gallery_features] = training(gallery);
 
-% La stringa flag verrà usata per ver verificare se l'utente vuole
-% continuare ad utilizzare il programma o meno
+% La variabile flag verrà utilizzata per ver verificare se l'utente vuole
+% continuare ad usufruire del programma o meno
 flag = '';
 while(strcmp(flag, 'esci')==0)    
    
-    [filename] = imgetfile(); %Scelta dell'immagine da GUI
+    [filename] = imgetfile(); %L'utente sceglie l'immagine da autenticare da GUI
     
-    %calcola la similarita' dell'immagine in input con ogni immagine nel
-    %training set
-    probe = imread(filename);
+   
+    probe = imread(filename); %Probe contiene l'immagine scelta dall'utente
     probe = preprocess(probe); %viene eseguito il preprocessing sull'immagine scelta dall'utente
-    [pathstr_notUsed,current_name] = fileparts(filename); %viene estratto il node del soggetto scelto dall'untete
+    [pathstr_notUsed,current_name] = fileparts(filename); %viene estratto il node del soggetto scelto dall'utente
     fprintf('Immagine scelta: %s \n',current_name);% e viene stampato a video
     
-    true_class = str2double(current_name(6:7)); % questa variabile conterrà il nome del soggetto scelto dall'utente
+    true_class = str2double(current_name(6:7)); % questa variabile conterrà il numero che identifica un soggetto (ad esempio: numer 09 identifica Simone Scemo)
     
+    %Vengono  estratte le features dell'immagine scelta dall'utente
     probe_features = get_features(evectors, mean_face, probe);
+    %calcola la similarita' dell'immagine in input con ogni immagine nel
+    %training set, e viene restituita l'immagine più simile, lo score
+    %ottenuto ed infine la classe che è stata riconosciuta
     [match_score, match_image, recognized_class] = recognize(gallery, classes, gallery_features, probe_features);    
         
     %mostra il risultato
